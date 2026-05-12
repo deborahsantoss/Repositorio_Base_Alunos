79baf511855df0133f65078ea0d82b631e835d13
deborahsantoss <deborahsantosdeoliveira3@gmail.com>
2026-05-12 08:48:56 -0300

    Add files via upload

---
+++ Modulo 1/Extra/Valor_maior.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+ 
+if numero1 == numero2:
+   print("Os numeros sao iguais! ")
+elif numero1 > numero2: 
+   print(f"(numero1) e maior que (numero2)!")
+else:
+   print(f"(numero2) e maior que (numero1)!")
